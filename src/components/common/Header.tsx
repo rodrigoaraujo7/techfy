b2f6c718cf7c04c9d96a5c34a4c391c19bee48ce
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-07-14 18:30:51 -0300

    feat: responsive header
    Creating the base responsive header with hamburguer menu handler

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
 import Image from "next/image";
 
+import { Sidebar } from "./Sidebar";
+
+import { Menu, User, ShoppingCart, Heart } from "lucide-react";
+
 export const Header = () => {
+  const [displaySidebar, setDisplaySidebar] = useState<boolean>(false);
+
   return (
-    <header className="w-full h-[88px] main-cell-position flex justify-between items-center py-4">
-      <Image src={"/logo.png"} alt="" width={96} height={32} />
-    </header>
+    <>
+      <header className="bg-white-100 w-full h-[88px] main-cell-position flex justify-between items-center py-7 px-4 xl:px-0">
+        <Image src={"/logo.png"} alt="" width={96} height={32} />
+
+        <div className=" items-center gap-6 hidden md:flex">
+          <Heart size={20} strokeWidth={1.5} className="cursor-pointer" />
+          <ShoppingCart
+            size={20}
+            strokeWidth={1.5}
+            className="cursor-pointer"
+            onClick={() => setDisplaySidebar(true)}
+          />
+          <User size={20} strokeWidth={1.5} className="cursor-pointer" />
+        </div>
+
+        <Menu
+          size={32}
+          strokeWidth={1.5}
+          className="cursor-pointer md:hidden"
+          onClick={() => setDisplaySidebar(true)}
+        />
+      </header>
+
+      {displaySidebar && <Sidebar />}
+    </>
   );
 };
---
+++ src/components/common/Sidebar.tsx
@@ -0,0 +1,9 @@
+export const Sidebar = () => {
+  return (
+    <div className="fixed inset-0 bg-[#00000032]">
+      <div className="absolute top-0 right-0 bottom-0 max-w-[342px] min-w-[300px] sm:w-[420px] bg-white-100 py-10 px-6">
+        <h1>teste</h1>
+      </div>
+    </div>
+  );
+};
