01e09d1d649dc2bcc0bde0426a39f697ac77b94e
rodrigoaraujo7 <dev.rodrigoaraujo@gmail.com>
2025-07-17 16:43:31 -0300

    feat: mobile grid
    Styling mobile grid section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,18 +28,81 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 grid-rows-[365px_365px_365px_365px] lg:grid-cols-4 lg:grid-rows-[328px_272px]">
+      <div className="grid grid-cols-1 grid-rows-4 md:grid-rows-2 md:grid-cols-2 lg:grid-cols-4 lg:grid-rows-[328px_272px]">
         <div className="hero-cell bg-white-500 lg:col-start-1 lg:col-end-2 lg:row-start-2 lg:row-end-3">
-          apple air pods max
+          <Image
+            src={"/images/apple-airpods-max.png"}
+            alt="apple-airpods-max-image"
+            width={200}
+            height={200}
+          />
+
+          <div className="flex flex-col justify-center gap-4 text-center max-w-[420] md:w-fit">
+            <h1 className="text-3xl font-light text-black-500">
+              Apple AirPods <span className="font-medium">Max</span>
+            </h1>
+            <h2 className="text-neutral-300">
+              {`Computational audio. Listen, it's powerful`}
+            </h2>
+          </div>
         </div>
+
         <div className="hero-cell bg-dark-700 lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-3">
-          apple vision pro
+          <Image
+            src={"/images/apple-vision-pro.png"}
+            alt="apple-vision-pro-image"
+            width={300}
+            height={200}
+          />
+
+          <div className="flex flex-col justify-center gap-4 text-center max-w-[420] md:w-fit">
+            <h1 className="text-3xl font-light text-white-100">
+              Apple Vision <span className="font-medium">Pro</span>
+            </h1>
+            <h2 className="text-neutral-300">
+              An immersive way to experience entertainment
+            </h2>
+          </div>
         </div>
+
         <div className="hero-cell bg-white-100 lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-2">
-          playstation 5
+          <Image
+            src={"/images/playstation.png"}
+            alt="playstation-image"
+            width={300}
+            height={300}
+          />
+
+          <div className="flex flex-col justify-center gap-4 text-center max-w-[420] md:w-fit">
+            <h1 className="text-3xl font-light text-black-500">
+              Playstation <span className="font-medium">5</span>
+            </h1>
+            <h2 className="text-neutral-300">
+              Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O
+              will redefine your PlayStation experience.
+            </h2>
+          </div>
         </div>
+
         <div className="hero-cell bg-white-500 lg:col-start-3 lg:col-end-5 lg:row-start-1 lg:row-end-3">
-          macbook pro
+          <Image
+            src={"/images/macbook-air.png"}
+            alt="macbook-image"
+            width={300}
+            height={200}
+          />
+
+          <div className="flex flex-col justify-center gap-4 text-center max-w-[420] md:w-fit">
+            <h1 className="text-3xl font-light text-black-500">
+              <span className="font-medium">Macbook</span> Air
+            </h1>
+            <h2 className="text-neutral-300">
+              The new 15-inch MacBook Air makes room for more of what you love
+              with a spacious Liquid Retina display.
+            </h2>
+
+            <Button variant="dark">Shop Now</Button>
+          </div>
         </div>
       </div>
     </section>
